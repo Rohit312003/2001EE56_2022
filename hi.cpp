67afe7f7477a65fdbd012324dee00c8a38f24127
Rohit Kumar <rohit312003@gmail.com>
2022-08-27 13:35:38 +0530

    Create hi.cpp
    added in 2001ee56_2022

---
+++ hi.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+#include<string>
+#include<math.h>
+#include<string.h>
+#include<sstream>
+#include<iostream>
+#include<vector>
+using namespace std;
+typedef long long int ll;
+#define MOD 1000000007;
+const int N = 1e5 + 10;
+const int INF = 1e9 + 10;
+vector<int> vis(N, 0);
+vector<int> dist(N, INF);
+ vector<pair<int, int>> g[N];
+int main()
+{
+   ios_base::sync_with_stdio(false);
+   cin.tie(NULL);
+   cout.tie(NULL);
+
+   ll t;
+   cin>>t;
+   while(t--)
+   {
+   
+   }
+   return 0;
+}
