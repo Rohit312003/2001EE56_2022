a94ccb864e8249ec08bfc4e0d5f810eff4999a2f
Rohit Kumar <rohit312003@gmail.com>
2022-09-28 20:47:05 +0530

    adding more comments
    explaining the code

--- tut02/tut02.py
+++ tut02/tut02.py
@@ -1,28 +1,14 @@
-# def octant_transition_count(mod=5000):
-# ###Code
-
-# from platform import python_version
-# ver = python_version()
-
-# if ver == "3.8.10":
-#     print("Correct Version Installed")
-# else:
-#     print("Please install 3.8.10. Instruction are present in the GitHub Repo/Webmail. Url: https://pastebin.com/nvibxmjw")
-
-# mod=5000
-# octant_transition_count(mod)
 
 from operator import concat
 import pandas as pd
 import openpyxl 
-
 import csv
 import numpy as np
 import os
 from openpyxl import load_workbook  
 wb = load_workbook('input_octant_transition_identify.xlsx')  
 sheet_obj = wb.active
-
+                                                                  #reading the input file and storing its value in the df 
 df = pd.read_excel('input_octant_transition_identify.xlsx')
 
 print("Column headings:")
@@ -30,23 +16,17 @@ print(df)
 meanU=df['U'].mean()
 meanV=df['V'].mean()
 meanW=df['W'].mean()
-#  making new colunms for U avg, V avg, W avg.
+                                                                  #  making new colunms for U avg, V avg, W avg.
 df['U avg']=meanU
 df['V avg']=meanV
 df['W avg']=meanW
-#  calculating error in U,V and W.
+                                                                  #  calculating error in U,V and W.
 df['U-Uavg']=df['U']-meanU
 df['V-Vavg']=df['V']-meanV
 df['W-Wavg']=df['W']-meanW
-# creating a list for octant values
-octant=[]
-
-
-#  calculating the Octant value .
-for (l, m, q) in zip(df['U-Uavg'],df['V-Vavg'],df['W-Wavg']):
-         
-     
-#   appanding the octannt values to the list
+                                                                  # creating a list for octant values
+octant=[]                                                         #  calculating the Octant value .
+for (l, m, q) in zip(df['U-Uavg'],df['V-Vavg'],df['W-Wavg']):     #   appanding the octannt values to the list
          if l>0 and m>0 and q<0:
              octant.append('-1')
          elif l>0 and m>0 and q>0:
@@ -62,23 +42,17 @@ for (l, m, q) in zip(df['U-Uavg'],df['V-Vavg'],df['W-Wavg']):
          elif l>=0 and m<=0 and q<0:
             octant.append('-4')
          elif l>0 and m<0 and q>0:
-             octant.append('4')
-   
-  
+             octant.append('4') 
 df['octant'] = octant
- 
-
-
- 
 leng=df['octant'].count()
 print(leng) 
-# Initialize matrix
+                                                                    # Initialize matrix
 matri = []
 
-  
-# For user input
+                                                                    #making list
+                                                                     
 a=[]
-      # A for loop for column entries
+                                                                    # making a row using list
 a.append('Octant ID')
 a.append('  1')
 a.append(' -1')
@@ -88,10 +62,11 @@ a.append('  3')
 a.append('   -3')
 a.append('  4')
 a.append('  -4')
-matri.append(a)
+                                                                    # appending list value in matrix its lok like a adding a rowwin exel
+matri.append(a)                                                     #making list 
 a=[]
 a.append('Overall')
-
+                                                                    # appending the value of counts of  octant values
 a.append(df['octant'].value_counts()['1'])
 a.append(df['octant'].value_counts()['-1'])
 a.append(df['octant'].value_counts()['2'])
@@ -100,21 +75,22 @@ a.append(df['octant'].value_counts()['3'])
 a.append(df['octant'].value_counts()['-3'])
 a.append(df['octant'].value_counts()['4'])
 a.append(df['octant'].value_counts()['-4'])
+                                                                # appending list value in matrix its lok like a adding a rowwin exel  
 matri.append(a)  
-# For printing the matrix
+                                                                # For printing the matrix
 for i in range(2):
     for j in range(9):
         print(matri[i][j], end = " ")
     print()
 
 
-# creating list of list name matri
+                                                                # creating list of list name matri
 
 # k=int (input("enter mod "))
-# giving the value of MOD  
+                                                                # giving the value of MOD  
 k=5000
 zo=k
-# printing the mod value
+                                                                # printing the mod value
 print("mod"+str(k))
 
 
@@ -164,20 +140,13 @@ for i in range(R):          # A for loop for row entries
       a.append(count)
      
     matrix.append(a)
-
-    
-      
-
 # For printing the matrix
 for i in range(R):
     for j in range(C):
         print(matrix[i][j], end = "    ")
     print()
 
-newtransmat=[]
-
-
-  
+newtransmat=[] 
 # For user input
 popoo=[]
 popoo4=[]
@@ -187,8 +156,6 @@ popoo4=[]
 popoo4.append(" ")
 newtransmat.append(popoo4)
 popoo.append("Overall Transition Count")
-
-
 newtransmat.append(popoo)
 a=[]
 
@@ -203,10 +170,6 @@ a.append('   -3')
 a.append('  4')
 a.append('  -4')
 newtransmat.append(a)
-
-
-
-
 for j in range(1,5):
     q=[]
     a=b=c=d=e=f=g=h=0
@@ -244,8 +207,7 @@ for j in range(1,5):
         if(df['octant'][k]==str(-j) and df['octant'][k+1]=='4'):
             gg=gg+1
         if(df['octant'][k]==str(-j) and df['octant'][k+1]=='-4'):
-            hh=hh+1
-        
+            hh=hh+1   
     q.append(str(j))
     q.append(str(a))
     q.append(str(b))
@@ -257,7 +219,6 @@ for j in range(1,5):
     q.append(str(h))
     newtransmat.append(q)
     y=[]
-    
     y.append(str(-j))
     y.append(str(aa))
     y.append(str(bb))
@@ -267,20 +228,10 @@ for j in range(1,5):
     y.append(str(ff))
     y.append(str(gg))
     y.append(str(hh))
-   
-    
     newtransmat.append(y)
-
-    
-
-
-
 for i in range(R):          # A for loop for row entries
     # creating a list
     a =[]
-     
-
-       
         # condition for last row of output to cheak the  length of input so it stop when input length reached
     if(i==R-1):
             popoo=[]
@@ -349,9 +300,7 @@ for i in range(R):          # A for loop for row entries
                         if(df['octant'][k]==str(-j) and df['octant'][k+1]=='4'):
                             gg=gg+1
                         if(df['octant'][k]==str(-j) and df['octant'][k+1]=='-4'):
-                            hh=hh+1
-                        
-                        
+                            hh=hh+1    
                     q.append(str(j))
                     q.append(str(a))
                     q.append(str(b))
@@ -363,7 +312,6 @@ for i in range(R):          # A for loop for row entries
                     q.append(str(h))
                     newtransmat.append(q)
                     y=[]
-                    
                     y.append(str(-j))
                     y.append(str(aa))
                     y.append(str(bb))
@@ -373,30 +321,16 @@ for i in range(R):          # A for loop for row entries
                     y.append(str(ff))
                     y.append(str(gg))
                     y.append(str(hh))
-                    newtransmat.append(y)
-
-                    
-    
-    
-                     
-                    
+                    newtransmat.append(y)                                  
 print(newtransmat)      
-    
-
-
-
-
 # temprorly saving matrices in file1.csv
 with open("file2.csv","w") as file1:
     csvWriter = csv.writer(file1,delimiter=',')
     csvWriter.writerows(matri)
     csvWriter.writerows(matrix)
     csvWriter.writerows(newtransmat)
-
-
 # reading the same file which you recently seved
 dt=pd.read_csv('file2.csv')
-
 # addding new column to the Output file
 df['_']=str(" ")
 df['Octant ID']=dt['Octant ID']
@@ -408,26 +342,13 @@ df['  3']=dt['  3']
 df['   -3']=dt['   -3']
 df['  4']=dt['  4']
 df['  -4']=dt['  -4']
-
 # removing the opend file1.csv file
 os.remove("file2.csv")
-#saving my output in csv
 for i in range(0,leng):
     if df['Octant ID'][i]=='mod ':
-        df['_'][i]='Input'
-        
-    
+        df['_'][i]='Input'  
     if df['Octant ID'][i]=='1':
          df['_'][i]='From'
     
-
-
-
-
-
-
-
-
-
 with pd.ExcelWriter('output_octant_transition_identify.xlsx') as writer:
     df.to_excel(writer, sheet_name='Sheet_name_1')
